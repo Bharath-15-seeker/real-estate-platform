4828ef666507b58473650f0a71d8d160ab37e4ee
Bharath <bharathkandasamy15@gmail.com>
2025-10-08 23:17:16 +0530

    Add executable permission for mvnw

--- src/main/java/com/realestate/real_estate_platform/config/WebConfig.java
+++ src/main/java/com/realestate/real_estate_platform/config/WebConfig.java
@@ -22,8 +22,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("https://property-hub2025.netlify.app")
                 .allowedOriginPatterns("http://localhost:63342","http://127.0.0.1:5500")
-
                 .allowedMethods("GET","POST", "PUT", "DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
